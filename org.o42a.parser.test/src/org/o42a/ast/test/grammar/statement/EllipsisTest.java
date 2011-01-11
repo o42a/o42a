8c011172a588d2adec22306032fcd1f8d60d95e7
lorus <lorus@localhost>
2011-01-11 16:33:59 +0600

    Parser: * Recognize HORIZONTAL ELLIPSIS (U+2026) as ellipsis within imperative code.

--- org.o42a.parser.test/src/org/o42a/ast/test/grammar/statement/EllipsisTest.java
+++ org.o42a.parser.test/src/org/o42a/ast/test/grammar/statement/EllipsisTest.java
@@ -39,19 +39,49 @@ public class EllipsisTest extends GrammarTestCase {
 	public void onlyEllipsis() {
 
 		final SentenceNode sentence = parse("...");
-		final EllipsisNode ellipsis = singleStatement(EllipsisNode.class, sentence);
+		final EllipsisNode ellipsis =
+			singleStatement(EllipsisNode.class, sentence);
 
 		assertNull(sentence.getMark());
 		assertRange(0, 3, ellipsis);
 		assertNull(ellipsis.getTarget());
-		assertThat(ellipsis.getMark().getType(), is(EllipsisNode.Mark.ELLIPSIS));
+		assertThat(
+				ellipsis.getMark().getType(),
+				is(EllipsisNode.Mark.ELLIPSIS));
+	}
+
+	@Test
+	public void onlyHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("\u2026");
+		final EllipsisNode ellipsis =
+			singleStatement(EllipsisNode.class, sentence);
+
+		assertNull(sentence.getMark());
+		assertRange(0, 1, ellipsis);
+		assertNull(ellipsis.getTarget());
+		assertThat(
+				ellipsis.getMark().getType(),
+				is(EllipsisNode.Mark.ELLIPSIS));
 	}
 
 	@Test
 	public void onlyTargetedEllipsis() {
 
 		final SentenceNode sentence = parse("... bar");
-		final EllipsisNode ellipsis = singleStatement(EllipsisNode.class, sentence);
+		final EllipsisNode ellipsis =
+			singleStatement(EllipsisNode.class, sentence);
+
+		assertNull(sentence.getMark());
+		assertEquals("bar", ellipsis.getTarget().getName());
+	}
+
+	@Test
+	public void onlyTargetedHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("\u2026 bar");
+		final EllipsisNode ellipsis =
+			singleStatement(EllipsisNode.class, sentence);
 
 		assertNull(sentence.getMark());
 		assertEquals("bar", ellipsis.getTarget().getName());
@@ -61,8 +91,10 @@ public class EllipsisTest extends GrammarTestCase {
 	public void untargetedEllipsis() {
 
 		final SentenceNode sentence = parse("foo ...");
-		final MemberRefNode statement = statement(MemberRefNode.class, sentence, 0, 2);
-		final EllipsisNode ellipsis = statement(EllipsisNode.class, sentence, 1, 2);
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
 
 		assertNull(sentence.getMark());
 		assertName("foo", statement);
@@ -71,11 +103,42 @@ public class EllipsisTest extends GrammarTestCase {
 	}
 
 	@Test
+	public void untargetedHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("foo \u2026");
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
+
+		assertNull(sentence.getMark());
+		assertName("foo", statement);
+		assertRange(4, 5, ellipsis);
+		assertNull(ellipsis.getTarget());
+	}
+
+	@Test
 	public void targetedEllipsis() {
 
 		final SentenceNode sentence = parse("foo ... bar");
-		final MemberRefNode statement = statement(MemberRefNode.class, sentence, 0, 2);
-		final EllipsisNode ellipsis = statement(EllipsisNode.class, sentence, 1, 2);
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
+
+		assertNull(sentence.getMark());
+		assertName("foo", statement);
+		assertEquals("bar", ellipsis.getTarget().getName());
+	}
+
+	@Test
+	public void targetedHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("foo \u2026 bar");
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
 
 		assertNull(sentence.getMark());
 		assertName("foo", statement);
@@ -86,8 +149,24 @@ public class EllipsisTest extends GrammarTestCase {
 	public void lastInProposition() {
 
 		final SentenceNode sentence = parse("foo ....");
-		final MemberRefNode statement = statement(MemberRefNode.class, sentence, 0, 2);
-		final EllipsisNode ellipsis = statement(EllipsisNode.class, sentence, 1, 2);
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
+
+		assertThat(sentence.getMark().getType(), is(SentenceType.PROPOSITION));
+		assertName("foo", statement);
+		assertNull(ellipsis.getTarget());
+	}
+
+	@Test
+	public void horizontalEllipsisLastInProposition() {
+
+		final SentenceNode sentence = parse("foo \u2026.");
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
 
 		assertThat(sentence.getMark().getType(), is(SentenceType.PROPOSITION));
 		assertName("foo", statement);
@@ -98,8 +177,24 @@ public class EllipsisTest extends GrammarTestCase {
 	public void repeat() {
 
 		final SentenceNode sentence = parse("foo ... bar.");
-		final MemberRefNode statement = statement(MemberRefNode.class, sentence, 0, 2);
-		final EllipsisNode ellipsis = statement(EllipsisNode.class, sentence, 1, 2);
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
+
+		assertThat(sentence.getMark().getType(), is(SentenceType.PROPOSITION));
+		assertName("foo", statement);
+		assertEquals("bar", ellipsis.getTarget().getName());
+	}
+
+	@Test
+	public void repeatByHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("foo \u2026 bar.");
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
 
 		assertThat(sentence.getMark().getType(), is(SentenceType.PROPOSITION));
 		assertName("foo", statement);
@@ -110,8 +205,24 @@ public class EllipsisTest extends GrammarTestCase {
 	public void stop() {
 
 		final SentenceNode sentence = parse("foo ... bar!");
-		final MemberRefNode statement = statement(MemberRefNode.class, sentence, 0, 2);
-		final EllipsisNode ellipsis = statement(EllipsisNode.class, sentence, 1, 2);
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
+
+		assertThat(sentence.getMark().getType(), is(SentenceType.CLAIM));
+		assertName("foo", statement);
+		assertEquals("bar", ellipsis.getTarget().getName());
+	}
+
+	@Test
+	public void stopByHorizontalEllipsis() {
+
+		final SentenceNode sentence = parse("foo \u2026 bar!");
+		final MemberRefNode statement =
+			statement(MemberRefNode.class, sentence, 0, 2);
+		final EllipsisNode ellipsis =
+			statement(EllipsisNode.class, sentence, 1, 2);
 
 		assertThat(sentence.getMark().getType(), is(SentenceType.CLAIM));
 		assertName("foo", statement);
