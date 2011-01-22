17f3906ad80ace4c5bb6a454c1caedaf8ecfcbef
lorus <lorus@localhost>
2011-01-22 14:46:38 +0600

    Build: * copyright.sh script no longer touches the files with correct copyright notice.

--- build/copyright_file.sh
+++ build/copyright_file.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+blue=$'\033[1;34m'
 green=$'\033[0;32m'
 red=$'\033[0;31m'
 white=$'\033[1;37m'
 dirname=`dirname $0`
 
+filepath="$1"
 
-( echo "Updating copyright of: ${green}$1${white}" && \
-    awk -v progname="$2" -f "${dirname}/copyright.awk" "$1" > "$1.licensed" && \
-    mv -f "$1.licensed" "$1" ) \
-    || ( echo "${red}Failed to process ${green}$1${white}" > 2 && exit 1 )
+echo -n "Updating copyright of: ${green}$1${white}"
+awk -v progname="$2" -f "${dirname}/copyright.awk" "$filepath" > "$filepath.licensed"
+
+status=$?
+
+if (( status == 42 )); then
+    echo "${blue} ... not changed${white}"
+    rm -f "$filepath.licensed"
+    exit 0
+fi
+if (( status == 0 )); then
+    echo "${blue} ... ${white}UPDATED"
+    mv -f "$filepath.licensed" "$filepath" && exit 0
+fi
+
+echo "${blue} ... ${red}FAILED${white}" > 2 && exit status
