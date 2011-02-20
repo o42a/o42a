8ad6a1f7f36b9eebdf09d5d0546e8b0d2f434539
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2011-02-20 11:36:33 +0600

    Compiler: + More logical operators tests.

---
+++ org.o42a.compiler.test/src/org/o42a/compiler/test/operator/LogicalDefinitionTest.java
@@ -0,0 +1,119 @@
+/*
+    Compiler Tests
+    Copyright (C) 2011 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.o42a.compiler.test.operator;
+
+import org.junit.Test;
+import org.o42a.compiler.test.CompilerTestCase;
+import org.o42a.core.artifact.object.Obj;
+import org.o42a.util.Source;
+
+
+public class LogicalDefinitionTest extends CompilerTestCase {
+
+	private Obj a;
+	private Obj b;
+	private Obj c;
+
+	@Test
+	public void isTrue() {
+		compile("A := void. B := ++a. C := b.");
+
+		assertTrueVoid(this.a);
+		assertTrueVoid(this.b);
+		assertTrueVoid(this.c);
+	}
+
+	@Test
+	public void notTrue() {
+		compile("A := void. B := --a. C := b.");
+
+		assertTrueVoid(this.a);
+		assertFalseVoid(this.b);
+		assertFalseVoid(this.c);
+	}
+
+	@Test
+	public void notFalse() {
+		compile("A := false. B := --a. C := b.");
+
+		assertFalseVoid(this.a);
+		assertTrueVoid(this.b);
+		assertTrueVoid(this.c);
+	}
+
+	@Test
+	public void known() {
+		compile(
+				"A := false.",
+				"B := +-a.",
+				"C := b.");
+
+		assertKnownValue(this.a.getValue());
+		assertFalseVoid(this.a);
+		assertTrueVoid(this.b);
+		assertTrueVoid(this.c);
+	}
+
+	@Test
+	public void notKnown() {
+		compile(
+				"A := void(False? = void).",
+				"B := +-a.",
+				"C := b.");
+
+		assertUnknownValue(this.a.getValue());
+		assertFalseVoid(this.b);
+		assertFalseVoid(this.c);
+	}
+
+	@Test
+	public void notUnknown() {
+		compile(
+				"A := void.",
+				"B := -+a.",
+				"C := b.");
+
+		assertKnownValue(this.a.getValue());
+		assertTrueVoid(this.a);
+		assertFalseVoid(this.b);
+		assertFalseVoid(this.c);
+	}
+
+	@Test
+	public void unknown() {
+		compile(
+				"A := void(False? = void).",
+				"B := -+a.",
+				"C := b.");
+
+		assertUnknownValue(this.a.getValue());
+		assertTrueVoid(this.b);
+		assertTrueVoid(this.c);
+	}
+
+	@Override
+	protected void compile(Source source) {
+		super.compile(source);
+		this.a = field("a").getArtifact().materialize();
+		this.b = field("b").getArtifact().materialize();
+		this.c = field("c").getArtifact().materialize();
+	}
+
+}
---
+++ org.o42a.compiler.test/src/org/o42a/compiler/test/operator/LogiclInheritanceTest.java
@@ -0,0 +1,108 @@
+/*
+    Compiler Tests
+    Copyright (C) 2011 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.o42a.compiler.test.operator;
+
+import org.junit.Test;
+import org.o42a.compiler.test.CompilerTestCase;
+import org.o42a.core.artifact.object.Obj;
+import org.o42a.core.member.field.Field;
+import org.o42a.util.Source;
+
+
+public class LogiclInheritanceTest extends CompilerTestCase {
+
+	private Obj aBar;
+	private Obj bBar;
+	private Obj cBar;
+
+	@Test
+	public void isTrue() {
+		compile(
+				"A := void(",
+				"  Foo := void.",
+				"  Bar := ++foo.",
+				").",
+				"B := a(Foo = false).",
+				"C := b.");
+
+		assertTrueVoid(this.aBar);
+		assertFalseVoid(this.bBar);
+		assertFalseVoid(this.cBar);
+	}
+
+	@Test
+	public void not() {
+		compile(
+				"A := void(",
+				"  Foo := void.",
+				"  Bar := --foo.",
+				").",
+				"B := a(Foo = false).",
+				"C := b.");
+
+		assertFalseVoid(this.aBar);
+		assertTrueVoid(this.bBar);
+		assertTrueVoid(this.cBar);
+	}
+
+	@Test
+	public void known() {
+		compile(
+				"A := void(",
+				"  Foo := false.",
+				"  Bar := +-foo.",
+				").",
+				"B := a(Foo = *(False? = void)).",
+				"C := b.");
+
+		assertTrueVoid(this.aBar);
+		assertFalseVoid(this.bBar);
+		assertFalseVoid(this.cBar);
+	}
+
+	@Test
+	public void unknown() {
+		compile(
+				"A := void(",
+				"  Foo := false.",
+				"  Bar := -+foo.",
+				").",
+				"B := a(Foo = *(False? = void)).",
+				"C := b.");
+
+		assertFalseVoid(this.aBar);
+		assertTrueVoid(this.bBar);
+		assertTrueVoid(this.cBar);
+	}
+
+	@Override
+	protected void compile(Source source) {
+		super.compile(source);
+
+		final Field<?> a = field("a");
+		final Field<?> b = field("b");
+		final Field<?> c = field("c");
+
+		this.aBar = field(a, "bar").getArtifact().materialize();
+		this.bBar = field(b, "bar").getArtifact().materialize();
+		this.cBar = field(c, "bar").getArtifact().materialize();
+	}
+
+}
