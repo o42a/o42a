744af3c2b775a02968952e734fd2ad68293a6bcf
lorus <lorus@localhost>
2011-02-25 12:18:51 +0600

    Work on tests.

--- org.o42a.test/compile.sh
+++ org.o42a.test/compile.sh
@@ -4,6 +4,6 @@ dir=`dirname $0`
 
 "$dir/../o42ac" "$dir/all_tests.o42a" \
 	--vmargs -Djava.library.path="$dir/../target" -ea:org.o42a... \
-	-- -Xlinker -rpath="$dir/target" -Xlinker --no-undefined \
+	-- -Xlinker -rpath="$dir/../target" -Xlinker --no-undefined \
 	-L${dir}/../target -lo42a \
 	-o"$dir/all_tests"  
