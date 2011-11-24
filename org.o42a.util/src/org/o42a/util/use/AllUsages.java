e555bd7e869237b7aeb09c8fca1b62810da7c773
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2011-11-24 11:56:18 +0600

    Utilities: + Introduce Usage class, introducing the usage scenario. It'll be possible to use a single Uses instance with multiple Usage scenarios instead of confusing Uses-per-scenario approach.

---
+++ org.o42a.util/src/org/o42a/util/use/AllUsages.java
@@ -0,0 +1,85 @@
+/*
+    Utilities
+    Copyright (C) 2011 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.o42a.util.use;
+
+import java.lang.reflect.Array;
+
+import org.o42a.util.ArrayUtil;
+
+
+public final class AllUsages<U extends Usage<U>> implements UseSelector<U> {
+
+	private final Class<? extends U> usageClass;
+	private final String name;
+	private U usages[];
+	private AlwaysUsed<U> alwaysUsed;
+	private NeverUsed<U> neverUsed;
+
+	public AllUsages(Class<? extends U> usageClass) {
+		this(usageClass, null);
+	}
+
+	@SuppressWarnings("unchecked")
+	public AllUsages(Class<? extends U> usageClass, String name) {
+		this.usageClass = usageClass;
+		this.name = name != null ? name : usageClass.getSimpleName();
+		this.usages = (U[]) Array.newInstance(usageClass, 0);
+	}
+
+	public final Class<? extends U> getUsageClass() {
+		return this.usageClass;
+	}
+
+	public final U[] usages() {
+		return this.usages;
+	}
+
+	public final Uses<U> alwaysUsed() {
+		if (this.alwaysUsed != null) {
+			return this.alwaysUsed;
+		}
+		return this.alwaysUsed = new AlwaysUsed<U>(this);
+	}
+
+	public final Uses<U> neverUsed() {
+		if (this.neverUsed != null) {
+			return this.neverUsed;
+		}
+		return this.neverUsed = new NeverUsed<U>(this);
+	}
+
+	@Override
+	public boolean acceptUsage(U usage) {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		if (this.name != null) {
+			return super.toString();
+		}
+		return this.name;
+	}
+
+	final void addUsage(U usage) {
+		this.usages = ArrayUtil.append(this.usages, usage);
+	}
+
+}
