fe329def48429e5cf8c4aa93f91ccee840501e61
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2012-03-22 15:47:20 +0700

    Core: Separate ObjectDefiner implementation from ObjectFieldVariant.

---
+++ org.o42a.core/src/org/o42a/core/object/impl/decl/ObjectDefinerImpl.java
@@ -0,0 +1,96 @@
+/*
+    Compiler Core
+    Copyright (C) 2010-2012 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.o42a.core.object.impl.decl;
+
+import org.o42a.core.member.Member;
+import org.o42a.core.member.field.Field;
+import org.o42a.core.member.field.ObjectDefiner;
+import org.o42a.core.object.type.Ascendants;
+import org.o42a.core.ref.type.StaticTypeRef;
+import org.o42a.core.ref.type.TypeRef;
+import org.o42a.core.st.sentence.BlockBuilder;
+import org.o42a.core.value.TypeParameters;
+
+
+final class ObjectDefinerImpl implements ObjectDefiner {
+
+	private final ObjectFieldVariant variant;
+	private final Ascendants implicitAscendants;
+	private Ascendants ascendants;
+
+	ObjectDefinerImpl(
+			ObjectFieldVariant variant,
+			Ascendants implicitAscendants,
+			Ascendants ascendants) {
+		this.variant = variant;
+		this.implicitAscendants = implicitAscendants;
+		this.ascendants = ascendants;
+	}
+
+	@Override
+	public final Field<?> getField() {
+		return this.variant.getField();
+	}
+
+	@Override
+	public final Ascendants getImplicitAscendants() {
+		return this.implicitAscendants;
+	}
+
+	public final Ascendants getAscendants() {
+		return this.ascendants;
+	}
+
+	@Override
+	public ObjectDefiner setAncestor(TypeRef explicitAncestor) {
+		this.ascendants = this.ascendants.setAncestor(explicitAncestor);
+		return this;
+	}
+
+	@Override
+	public ObjectDefiner setTypeParameters(TypeParameters typeParameters) {
+		this.ascendants = this.ascendants.setTypeParameters(typeParameters);
+		return this;
+	}
+
+	@Override
+	public ObjectDefiner addExplicitSample(StaticTypeRef explicitAscendant) {
+		this.ascendants = this.ascendants.addExplicitSample(explicitAscendant);
+		return this;
+	}
+
+	@Override
+	public ObjectDefiner addImplicitSample(StaticTypeRef implicitAscendant) {
+		this.ascendants = this.ascendants.addImplicitSample(implicitAscendant);
+		return this;
+	}
+
+	@Override
+	public ObjectDefiner addMemberOverride(Member overriddenMember) {
+		this.ascendants = this.ascendants.addMemberOverride(overriddenMember);
+		return this;
+	}
+
+	@Override
+	public void define(BlockBuilder definitions) {
+		definitions.buildBlock(this.variant.getContent());
+	}
+
+}
