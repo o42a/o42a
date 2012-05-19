1d47a344aeecef27d90c7b42026533ead24a4384
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2012-05-19 20:12:33 +0700

    RT: Start the implementation of garbage collector.

--- org.o42a.rt/include/o42a/memory/gc.h
+++ org.o42a.rt/include/o42a/memory/gc.h
@@ -22,26 +22,102 @@
 
 #include "o42a/types.h"
 
-
+/**
+ * Obtains the garbage-allocated data block by data pointer.
+ *
+ * \param mem a pointer to the data start.
+ */
 #define o42a_gc_blockof(mem) \
 	((o42a_gc_block_t*) \
 			(((void*) (mem)) - offsetof(struct _o42a_gc_block, data)))
 
 typedef struct o42a_gc_block o42a_gc_block_t;
 
+typedef struct o42a_gc_use o42a_gc_use_t;
+
+/**
+ * A garbage-collected block of data.
+ *
+ * The data itself resides in memory after this structure and is aligned by the
+ * biggest alignment.
+ */
 struct o42a_gc_block {
 
-	o42a_gc_block_t *prev;
-
-	o42a_gc_block_t *next;
-
+	/**
+	 * An atomic (spin) lock of this block.
+	 *
+	 * A lock should be acquired with o42a_gc_lock_block prior to any operations
+	 * on this block and released after that with o42a_gc_unlock_block.
+	 */
 	uint8_t lock;
 
-	uint8_t flags;
+	/** A GC list identifier this block belongs to. */
+	uint8_t list;
 
-	uint8_t even_flags;
+	/** Block status flags. */
+	uint16_t flags;
 
-	uint8_t odd_flags;
+	/** A data describing the details of this block belonging to a GC list. */
+	union {
+
+		/**
+		 * This variant is used when the block belongs to a general GC list.
+		 */
+		struct {
+
+			/**
+			 * Previous block in the same list, or NULL if this is a first one.
+			 */
+			o42a_gc_block_t *prev;
+
+			/**
+			 * Next block in the same list, or NULL if this is a last one.
+			 */
+			o42a_gc_block_t *next;
+
+		} list;
+
+		/**
+		 * This variant is used when the block is used by some thread.
+		 */
+		struct {
+
+			/**
+			 * A pointer to the first block use descriptor.
+			 */
+			o42a_gc_use_t *first;
+
+		} uses;
+
+	} belonging;
+
+};
+
+/**
+ * The descriptor of the use of data block by some thread.
+ *
+ * This structure should be allocated on stack and the block field should be
+ * filled with a pointer to the block to use. The use can be claimed with
+ * o42a_gc_use function and freed with o42a_gc_unuse one.
+ *
+ * The same block can be used by multiple threads, thats one a list is needed.
+ */
+struct o42a_gc_use {
+
+	/**
+	 * The used data block.
+	 */
+	o42a_gc_block_t *block;
+
+	/**
+	 * Previous use descriptor in the list, or NULL if this is a first one.
+	 */
+	o42a_gc_use_t *prev;
+
+	/**
+	 * Next use descriptor in the list, or NULL if this is a last one.
+	 */
+	o42a_gc_use_t *next;
 
 };
 
@@ -57,16 +133,74 @@ struct _o42a_gc_block {
 extern "C" {
 #endif
 
+/**
+ * Allocates a garbage-collected block of data.
+ *
+ * The allocated block won't be added to any GC list automatically.
+ * Use o42a_gc_use right after allocation in order to do that.
+ *
+ * \param size the size of data to allocate, excluding the block header.
+ *
+ * \return data block pointer of NULL if allocation failed.
+ */
+o42a_gc_block_t *o42a_gc_block_alloc(O42A_DECLS size_t);
 
-o42a_gc_block_t *o42a_gc_balloc(O42A_DECLS size_t);
-
+/**
+ * Allocates a garbage-collected data.
+ *
+ * This function calls o42a_gc_balloc to allocate the data block and, in
+ * contrast to it, returns a pointer to the allocated data instead of a pointer
+ * to the data block.
+ *
+ * \param size the size of data to allocate.
+ *
+ * \return allocated data pointer or NULL if allocation failed.
+ */
 void *o42a_gc_alloc(O42A_DECLS size_t);
 
+/**
+ * Frees a garbage-collected data block previously allocated with o42a_gc_alloc
+ * or o42a_gc_balloc.
+ *
+ * \param the data block to free.
+ */
 void o42a_gc_free(O42A_DECLS o42a_gc_block_t *);
 
+/**
+ * Locks the GC data block.
+ */
+void o42a_gc_lock_block(O42A_DECLS o42a_gc_block_t *);
+
+/**
+ * Unlocks the GC data block.
+ */
+void o42a_gc_unlock_block(O42A_DECLS o42a_gc_block_t *);
+
+/**
+ * Declares the data block is used by current thread.
+ *
+ * While the data block is in use, it can not be considered a garbage and thus
+ * can not be deallocated.
+ *
+ * This is an atomic operation. The block should not be locked by current
+ * thread.
+ */
+void o42a_gc_use(O42A_DECLS o42a_gc_use_t *);
+
+/**
+ * Releases the use of data block.
+ *
+ * If this function releases the last use of the data block, then this data
+ * block will be submitted to GC and thus became a subject of garbage
+ * collection.
+ *
+ * This is an atomic operation. The block should not be locked by current
+ * thread.
+ */
+void o42a_gc_unuse(O42A_DECLS o42a_gc_use_t *);
 
 #ifdef __cplusplus
-}
+} /* externd "C" */
 #endif
 
 
--- org.o42a.rt/src/memory/gc.c
+++ org.o42a.rt/src/memory/gc.c
@@ -19,10 +19,124 @@
 */
 #include "o42a/memory/gc.h"
 
+#include <sched.h>
 #include <stdlib.h>
 
 #include "o42a/error.h"
 
+
+/**
+ * GC list identifiers.
+ *
+ * These values can be used to fill o42a_gc_block.list field.
+ */
+enum o42a_gc_lists {
+
+	/** No list. This is only possible for newly created blocks. */
+	O42A_LIST_NONE = 0,
+
+	/**
+	 * A "gray" list flag. The "gray" list is a list of data objects known to be
+	 * used and thus couldn't be deallocated.
+	 */
+	O42A_LIST_GRAY_FLAG = 0x10,
+
+	/**
+	 * A list of data blocks used by some thread(s).
+	 *
+	 * A data block from this list travels to the "white" list once no longer
+	 * used.
+	 *
+	 * This, along with O42A_LIST_STATIC forms a "gray" GC list.
+	 */
+	O42A_LIST_USED = O42A_LIST_GRAY_FLAG | 0,
+
+	/**
+	 * A list of statically allocated data blocks.
+	 *
+	 * This, along with O42A_LIST_USED forms a "gray" GC list.
+	 */
+	O42A_LIST_STATIC = O42A_LIST_GRAY_FLAG | 1,
+
+	/**
+	 * A "while" list flag of all the remaining data blocks.
+	 *
+	 * There are two such lists: an "even" and an "odd" one. The actual list
+	 * identifier can be constructed by logical OR of this flag and an oddity
+	 * bit.
+	 *
+	 * These list contain a data blocks not present in any of the previous
+	 * ones and not considered as used by current GC iteration.
+	 *
+	 * Two "white" lists are handled by GC in turn, one at a time. For each list
+	 * GC sequentially performs a "mark" pass, and then - a "sweep" one.
+	 *
+	 * At the "mark" GC pass, each data block encountered used travels to the
+	 * list's counterpart, i.e. from "even" to "odd" one and vice versa.
+	 *
+	 * At the "sweep" GC pass, all the data blocks remaining in the list
+	 * are freed.
+	 *
+	 * After that the GC switches to the list's counterpart and performs
+	 * the steps again.
+	 */
+	O42A_LIST_WHITE_FLAG = 0x20,
+
+	/**
+	 * An "even" "white" list.
+	 */
+	O42A_LIST_EVEN = O42A_LIST_WHITE_FLAG | 0,
+
+	/**
+	 * An "odd" "white" list.
+	 */
+	O42A_LIST_ODD = O42A_LIST_WHITE_FLAG | 1,
+
+};
+
+/**
+ * A garbage-collected data block flags.
+ *
+ * These flags are used to fill the o42a_gc_block.flags field.
+ */
+enum o42a_gc_block_flags {
+
+	/**
+	 * The data block is checked a some iteration.
+	 *
+	 * Shift this flag left by oddity bits to get the flag value for the
+	 * corresponding "even" or "odd" list.
+	 */
+	O42A_GC_BLOCK_CHECKED
+
+};
+
+typedef struct o42a_gc_list {
+
+	o42a_gc_block_t *first;
+
+	o42a_gc_block_t *last;
+
+} o42a_gc_list_t;
+
+
+/**
+ * GC oddity, i.e. which of the "white" lists is currently processed.
+ */
+static volatile unsigned gc_oddity;
+
+/**
+ * GC lock.
+ *
+ * Should be acquired with o42a_gc_lock for GC list modifications GC oddity
+ * switch.
+ */
+static int gc_lock;
+
+
+static o42a_gc_list_t gc_used_list;
+
+static o42a_gc_list_t gc_white_lists[2];
 
 o42a_gc_block_t *o42a_gc_balloc(
 		O42A_PARAMS
@@ -37,29 +151,215 @@ o42a_gc_block_t *o42a_gc_balloc(
 		exit(EXIT_FAILURE);
 	}
 
-	block->prev = NULL;
-	block->next = NULL;
 	block->lock = 0;
+	block->list = O42A_LIST_NONE;
 	block->flags = 0;
-	block->even_flags = 0;
-	block->odd_flags = 0;
+	block->belonging.list.prev = NULL;
+	block->belonging.list.next = NULL;
 
 	O42A_RETURN block;
 }
 
 inline void *o42a_gc_alloc(O42A_PARAMS const size_t size) {
 	O42A_ENTER(return NULL);
-
 	struct _o42a_gc_block *const block =
 			(struct _o42a_gc_block *) O42A(o42a_gc_balloc(O42A_ARGS size));
-
 	O42A_RETURN (void*) &block->data;
 }
 
-void o42a_gc_free(O42A_PARAMS o42a_gc_block_t *const block) {
+inline void o42a_gc_free(O42A_PARAMS o42a_gc_block_t *const block) {
+	O42A_ENTER(return);
+	O42A(free(block));
+	O42A_RETURN;
+}
+
+
+inline void o42a_gc_lock_block(O42A_PARAMS o42a_gc_block_t *const block) {
+	O42A_ENTER(return);
+	while (__sync_val_compare_and_swap(&block->lock, 0, 1)) {
+		O42A(sched_yield());
+	}
+	__sync_lock_test_and_set(&block->lock, 1);
+	O42A_RETURN;
+}
+
+inline void o42a_gc_unlock_block(O42A_PARAMS o42a_gc_block_t *const block) {
+	O42A_ENTER(return);
+	__sync_lock_release(&block->lock);
+	block->lock = 0;
+	O42A_RETURN;
+}
+
+/**
+ * Locks GC for list modifications.
+ */
+static inline void o42a_gc_lock(O42A_PARAM) {
+	O42A_ENTER(return);
+	while (__sync_val_compare_and_swap(&gc_lock, 0, 1)) {
+		O42A(sched_yield());
+	}
+	__sync_lock_test_and_set(&gc_lock, 1);
+	O42A_RETURN;
+}
+
+/**
+ * Unlocks GC previously locked with gc_lock.
+ */
+static inline void o42a_gc_unlock(O42A_PARAM) {
+	O42A_ENTER(return);
+	__sync_lock_release(&gc_lock, 0);
+	gc_lock = 0;
+	O42A_RETURN;
+}
+
+static inline void o42a_gc_list_add(
+		O42A_PARAMS
+		o42a_gc_list_t *const list,
+		o42a_gc_block_t *const block) {
 	O42A_ENTER(return);
 
-	O42A(free(block));
+	o42a_gc_block_t *const last = list->last;
+
+	block->belonging.list.prev = last;
+	block->belonging.list.next = NULL;
+	if (!last) {
+		list->first = block;
+	}
+	list->last = block;
+
+	O42A_RETURN;
+}
+
+static inline void o42a_gc_list_remove(
+		O42A_PARAMS
+		o42a_gc_list_t *const list,
+		o42a_gc_block_t *const block) {
+	O42A_ENTER(return);
+
+	o42a_gc_block_t *const prev = block->belonging.list.prev;
+	o42a_gc_block_t *const next = block->belonging.list.next;
+
+	if (next) {
+		next->belonging.list.prev = prev;
+	} else {
+		list->last = prev;
+	}
+	if (prev) {
+		prev->belonging.list.next = next;
+	} else {
+		list->first = next;
+	}
+
+	O42A_RETURN;
+}
+
+static inline void o42a_gc_block_uncheck(O42A_PARAMS o42a_gc_block_t *block) {
+	O42A_ENTER(return);
+	block->flags &= ~(O42A_GC_BLOCK_CHECKED << (gc_oddity ^ 1));
+	O42A_RETURN;
+}
+
+void o42a_gc_use(O42A_PARAMS o42a_gc_use_t *const use) {
+	O42A_ENTER(return);
+
+	o42a_gc_block_t *const block = use->block;
+
+	O42A(o42a_gc_lock_block(O42A_ARGS block));
+
+	switch (block->list) {
+	case O42A_LIST_NONE:
+		// Not initialized yet.
+		// Create the uses list.
+		block->list = O42A_LIST_USED;
+		block->belonging.uses.first = use;
+
+		O42A(o42a_gc_lock(O42A_ARG));
+		// Add to the used list.
+		O42A(o42a_gc_list_add(O42A_ARGS &gc_used_list, block));
+		O42A(o42a_gc_unlock(O42A_ARG));
+
+		O42A(o42a_gc_unlock_block(O42A_ARGS block));
+
+		O42A_RETURN;
+	case O42A_LIST_STATIC:
+		// Static data is "grey".
+		O42A(o42a_gc_unlock_block(O42A_ARGS block));
+		O42A_RETURN;
+	case O42A_LIST_USED:
+		// Already used by some thread.
+		{
+			// Extend the uses list.
+			o42a_gc_use_t *const first = block->belonging.uses.first;
+
+			block->list = O42A_LIST_USED;
+			block->belonging.uses.first = use;
+			first->prev = use;
+			use->next = first;
+		}
+
+		O42A(o42a_gc_unlock_block(O42A_ARGS block));
+
+		O42A_RETURN;
+	}
+
+	// Data block is in the white list.
+	o42a_gc_list_t *const white_list = &gc_white_lists[block->list & 1];
+
+	O42A(o42a_gc_lock(O42A_ARG));
+	// Remove from the white list.
+	O42A(o42a_gc_list_remove(O42A_ARGS white_list, block));
+	// Add to the used list.
+	O42A(o42a_gc_list_add(O42A_ARGS &gc_used_list, block));
+	// Drop the checked flag for the next oddity.
+	O42A(o42a_gc_block_uncheck(O42A_ARGS block));
+	O42A(o42a_gc_unlock(O42A_ARG));
+
+	// Create the use list.
+	block->list = O42A_LIST_USED;
+	block->belonging.uses.first = use;
+
+	O42A(o42a_gc_unlock_block(O42A_ARGS block));
+
+	O42A_RETURN;
+}
+
+void o42a_gc_unuse(O42A_PARAMS o42a_gc_use_t *const use) {
+	O42A_ENTER(return);
+
+	o42a_gc_block_t *const block = use->block;
+
+	O42A(o42a_gc_lock_block(O42A_ARGS block));
+
+	// Reduce the uses list.
+	o42a_gc_use_t *const prev = use->prev;
+	o42a_gc_use_t *const next = use->next;
+
+	if (prev) {
+		prev->next = next;
+	} else {
+		block->belonging.uses.first = next;
+	}
+	if (next) {
+		next->prev = prev;
+		O42A(o42a_gc_unlock_block(O42A_ARGS block));
+		O42A_RETURN;
+	}
+	if (prev) {
+		O42A(o42a_gc_unlock_block(O42A_ARGS block));
+		O42A_RETURN;
+	}
+
+	// Not used any more.
+	O42A(o42a_gc_lock(O42A_ARG));
+	// Remove from used list.
+	O42A(o42a_gc_list_remove(O42A_ARGS &gc_used_list, block));
+	// Add to the next oddity white list.
+	O42A(o42a_gc_list_add(O42A_ARGS &gc_white_lists[gc_oddity ^ 1], block));
+	// Drop the checked flag for the next oddity.
+	O42A(o42a_gc_block_uncheck(O42A_ARGS block));
+	O42A(o42a_gc_unlock(O42A_ARG));
+
+	O42A(o42a_gc_unlock_block(O42A_ARGS block));
 
 	O42A_RETURN;
 }
