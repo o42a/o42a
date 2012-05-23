acdc45c64d89ad147b8b15e0077ea23bc07773bc
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2012-05-23 16:51:29 +0700

    RT: Add the tests build and execution rules to Makefile.

---
+++ org.o42a.rt/test/gc.c
@@ -0,0 +1,26 @@
+/*
+    Run-Time Library Tests
+    Copyright (C) 2012 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "o42a/memory/gc.h"
+
+const struct o42a_dbg o42a_dbg;
+
+int main(int argc, char **argv) {
+	return 0;
+}
