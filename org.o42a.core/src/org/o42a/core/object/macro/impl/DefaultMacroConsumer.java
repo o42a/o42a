48aabceb663d43b3ec0e1c0aeb9b76120f2c3910
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2012-08-23 14:20:21 +0700

    Core: Implement the default macro consumer.

--- org.o42a.core/src/org/o42a/core/object/macro/impl/DefaultMacroConsumer.java
+++ org.o42a.core/src/org/o42a/core/object/macro/impl/DefaultMacroConsumer.java
@@ -17,21 +17,41 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package org.o42a.core.object.macro;
+package org.o42a.core.object.macro.impl;
 
-import org.o42a.core.object.macro.impl.DefaultMacroConsumer;
+import static org.o42a.core.object.macro.MacroExpansionLogger.DEFAULT_MACRO_EXPANSION_LOGGER;
+
+import org.o42a.core.object.macro.MacroConsumer;
+import org.o42a.core.object.macro.MacroExpansionLogger;
 import org.o42a.core.ref.Consumer;
 import org.o42a.core.ref.Ref;
+import org.o42a.core.ref.path.PathTemplate;
 
 
-public interface MacroConsumer {
+public class DefaultMacroConsumer implements Consumer, MacroConsumer {
 
-	Consumer DEFAULT_CONSUMER = DefaultMacroConsumer.INSTANCE;
+	public static final DefaultMacroConsumer INSTANCE =
+			new DefaultMacroConsumer();
 
-	MacroConsumer DEFAULT_MACRO_CONSUMER = DefaultMacroConsumer.INSTANCE;
+	private DefaultMacroConsumer() {
+	}
 
-	MacroExpansionLogger getExpansionLogger();
+	@Override
+	public MacroConsumer expandMacro(
+			Ref macroRef,
+			PathTemplate template,
+			Ref expansion) {
+		return this;
+	}
 
-	Ref expandMacro(Ref macroExpansion);
+	@Override
+	public MacroExpansionLogger getExpansionLogger() {
+		return DEFAULT_MACRO_EXPANSION_LOGGER;
+	}
+
+	@Override
+	public Ref expandMacro(Ref macroExpansion) {
+		return macroExpansion;
+	}
 
 }
