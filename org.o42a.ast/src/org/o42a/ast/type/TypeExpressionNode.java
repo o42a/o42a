ff29c143da9a9a786bc205587b0ef3b64c76007a
lorus <lorus@localhost>
2012-08-24 14:34:28 +0700

    Grammar: The type parameter (e.g. link interface) may contain arbitrary macro expansion expression (including phrases). Such expressions exposed with ExpressionTypeNode.

---
+++ org.o42a.ast/src/org/o42a/ast/type/TypeExpressionNode.java
@@ -0,0 +1,67 @@
+/*
+    Abstract Syntax Tree
+    Copyright (C) 2012 Ruslan Lopatin
+
+    This file is part of o42a.
+
+    o42a is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    o42a is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.o42a.ast.type;
+
+import org.o42a.ast.AbstractNode;
+import org.o42a.ast.NodeVisitor;
+import org.o42a.ast.expression.ExpressionNode;
+import org.o42a.ast.expression.ExpressionNodeVisitor;
+import org.o42a.ast.statement.StatementNodeVisitor;
+
+
+public class TypeExpressionNode extends AbstractNode implements TypeNode {
+
+	private final ExpressionNode expression;
+
+	public TypeExpressionNode(ExpressionNode expression) {
+		super(expression.getStart(), expression.getEnd());
+		this.expression = expression;
+	}
+
+	public final ExpressionNode getExpression() {
+		return this.expression;
+	}
+
+	@Override
+	public <R, P> R accept(NodeVisitor<R, P> visitor, P p) {
+		return visitor.visitTypeExpression(this, p);
+	}
+
+	@Override
+	public <R, P> R accept(TypeNodeVisitor<R, P> visitor, P p) {
+		return visitor.visitTypeExpression(this, p);
+	}
+
+	@Override
+	public <R, P> R accept(ExpressionNodeVisitor<R, P> visitor, P p) {
+		return getExpression().accept(visitor, p);
+	}
+
+	@Override
+	public <R, P> R accept(StatementNodeVisitor<R, P> visitor, P p) {
+		return getExpression().accept(visitor, p);
+	}
+
+	@Override
+	public void printContent(StringBuilder out) {
+		this.expression.printContent(out);
+	}
+
+}
