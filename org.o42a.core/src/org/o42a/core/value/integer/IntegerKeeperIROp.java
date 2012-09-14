fdf36a11f513704ab2f54d2a34886ef2565c3bfa
lorus <lorus@localhost>
2012-09-14 12:12:24 +0700

    IR: Implement the integer keeper.

--- org.o42a.core/src/org/o42a/core/value/integer/IntegerKeeperIROp.java
+++ org.o42a.core/src/org/o42a/core/value/integer/IntegerKeeperIROp.java
@@ -19,7 +19,16 @@
 */
 package org.o42a.core.value.integer;
 
+import static java.lang.Integer.numberOfTrailingZeros;
+import static org.o42a.codegen.code.op.Atomicity.ATOMIC;
+import static org.o42a.core.ir.op.CodeDirs.codeDirs;
+import static org.o42a.core.ir.value.Val.VAL_CONDITION;
+import static org.o42a.core.ir.value.Val.VAL_INDEFINITE;
+import static org.o42a.core.ir.value.ValHolderFactory.TEMP_VAL_HOLDER;
+
+import org.o42a.codegen.code.Block;
 import org.o42a.codegen.code.Code;
+import org.o42a.codegen.code.CondBlock;
 import org.o42a.codegen.code.backend.StructWriter;
 import org.o42a.codegen.code.op.Int64recOp;
 import org.o42a.codegen.code.op.Int8recOp;
@@ -30,6 +39,8 @@ import org.o42a.core.ir.object.state.KeeperOp;
 import org.o42a.core.ir.op.CodeDirs;
 import org.o42a.core.ir.op.ValDirs;
 import org.o42a.core.ir.value.ValOp;
+import org.o42a.core.ref.Ref;
+import org.o42a.core.value.ValueStruct;
 import org.o42a.util.string.ID;
 
 
@@ -54,14 +65,89 @@ final class IntegerKeeperIROp extends KeeperIROp<IntegerKeeperIROp> {
 
 	@Override
 	protected void writeCond(KeeperOp keeper, CodeDirs dirs) {
-		// TODO Auto-generated method stub
 
+		final Ref value = keeper.getKeeper().getValue();
+		final ValueStruct<?, ?> valueStruct =
+				value.valueStruct(value.getScope());
+		final ValDirs valDirs = dirs.value(valueStruct, TEMP_VAL_HOLDER);
+
+		writeValue(keeper, valDirs);
+
+		valDirs.done();
 	}
 
 	@Override
 	protected ValOp writeValue(KeeperOp keeper, ValDirs dirs) {
-		// TODO Auto-generated method stub
-		return null;
+
+		final Block code = dirs.code();
+		final Int8recOp flags = flags(null, code);
+		final Int64recOp value = value(null, code);
+
+		code.acquireBarrier();
+
+		final CondBlock isSet =
+				flags.load(null, code)
+				.lowestBit(null, code)
+				.branch(code, "is_set", "not_set");
+
+		// A non-false value is already stored in keeper.
+		final ValOp value1 =
+				dirs.value().store(isSet, value.load(null, isSet));
+
+		isSet.go(code.tail());
+
+		// Value is either indefinite or false.
+		final Block notSet = isSet.otherwise();
+
+		final CondBlock eval =
+				flags.load(null, notSet)
+				.lshr(null, notSet, numberOfTrailingZeros(VAL_INDEFINITE))
+				.lowestBit(null, notSet)
+				.branch(notSet, "eval", "false");
+
+		// Evaluate and store the value.
+		final Block fail = eval.addBlock("fail");
+		final ValDirs valDirs =
+				codeDirs(dirs.getBuilder(), eval, fail.head())
+				.value(dirs);
+		final ValOp value21 =
+				keeper.getKeeper()
+				.getValue()
+				.op(keeper.host())
+				.writeValue(valDirs);
+
+		value.store(
+				eval,
+				value21.rawValue(null, eval).load(null, eval),
+				ATOMIC);
+		eval.releaseBarrier();
+		flags.store(eval, eval.int8((byte) VAL_CONDITION), ATOMIC);
+
+		valDirs.done().code().go(notSet.tail());
+
+		if (fail.exists()) {
+			// Value evaluation failed. Store false.
+			fail.releaseBarrier();
+			flags.store(fail, fail.int8((byte) 0), ATOMIC);
+			fail.go(notSet.tail());
+		}
+
+		// Value is false.
+		final Block falseVal = eval.otherwise();
+		final ValOp value22 =
+				dirs.getValueStruct()
+				.falseValue()
+				.op(dirs.getBuilder(), falseVal);
+
+		falseVal.go(notSet.tail());
+
+		// Evaluation or false result.
+		final ValOp value2 = value21.phi(null, notSet, value22);
+
+		notSet.go(code.tail());
+
+		// Net result.
+		return value1.phi(null, code, value2);
 	}
 
 	@Override
