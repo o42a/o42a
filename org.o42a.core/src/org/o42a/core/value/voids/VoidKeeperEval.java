a1e7dae2338d7afe5b4d47c5ece18a4f4806535d
lorus <lorus@localhost>
2012-09-15 22:32:13 +0700

    IR: Move the commonly used keeper evaluation logic to FlaggedKeeperEval. Correct the memory barrier acquirement time.

--- org.o42a.core/src/org/o42a/core/value/voids/VoidKeeperEval.java
+++ org.o42a.core/src/org/o42a/core/value/voids/VoidKeeperEval.java
@@ -19,50 +19,47 @@
 */
 package org.o42a.core.value.voids;
 
+import static org.o42a.codegen.code.op.Atomicity.ACQUIRE_RELEASE;
+import static org.o42a.core.ir.value.Val.VAL_CONDITION;
+import static org.o42a.core.value.Value.voidValue;
+
 import org.o42a.codegen.code.Code;
-import org.o42a.codegen.code.backend.StructWriter;
 import org.o42a.codegen.code.op.Int8recOp;
-import org.o42a.core.ir.object.ObjectOp;
-import org.o42a.core.ir.object.op.ObjHolder;
-import org.o42a.core.ir.object.state.KeeperIROp;
+import org.o42a.core.ir.object.state.FlaggedKeeperEval;
 import org.o42a.core.ir.object.state.KeeperOp;
-import org.o42a.core.ir.op.CodeDirs;
 import org.o42a.core.ir.op.ValDirs;
 import org.o42a.core.ir.value.ValOp;
-import org.o42a.util.string.ID;
 
 
-final class VoidKeeperIROp extends KeeperIROp<VoidKeeperIROp> {
+final class VoidKeeperEval extends FlaggedKeeperEval {
 
-	VoidKeeperIROp(StructWriter<VoidKeeperIROp> writer) {
-		super(writer);
+	private final VoidKeeperIROp op;
+
+	VoidKeeperEval(KeeperOp keeper, VoidKeeperIROp op) {
+		super(keeper);
+		this.op = op;
 	}
 
 	@Override
-	public final VoidKeeperIRType getType() {
-		return (VoidKeeperIRType) super.getType();
-	}
-
-	public final Int8recOp flags(ID id, Code code) {
-		return int8(id, code, getType().flags());
+	protected Int8recOp flagsRec(Code code) {
+		return this.op.flags(null, code);
 	}
 
 	@Override
-	protected void writeCond(KeeperOp keeper, CodeDirs dirs) {
-		new VoidKeeperEval(keeper, this).writeCond(dirs);
+	protected ValOp loadValue(ValDirs dirs, Code code) {
+		return voidValue().op(dirs.getBuilder(), code);
 	}
 
 	@Override
-	protected ValOp writeValue(KeeperOp keeper, ValDirs dirs) {
-		return new VoidKeeperEval(keeper, this).writeValue(dirs);
+	protected void updateValue(Code code, ValOp newValue) {
 	}
 
 	@Override
-	protected ObjectOp dereference(
-			KeeperOp keeper,
-			CodeDirs dirs,
-			ObjHolder holder) {
-		throw new UnsupportedOperationException();
+	protected void updateCondition(Code code, boolean condition) {
+		flagsRec().store(
+				code,
+				code.int8(condition ? (byte) VAL_CONDITION : 0),
+				ACQUIRE_RELEASE);
 	}
 
 }
