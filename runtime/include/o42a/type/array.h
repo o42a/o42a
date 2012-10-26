a3ba26517f7078500291ac2a2679520abeb652d9
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2012-10-26 15:22:22 +0700

    RT: Implement an array of duplicates construction.

--- runtime/include/o42a/type/array.h
+++ runtime/include/o42a/type/array.h
@@ -45,9 +45,40 @@ extern const o42a_val_type_t o42a_val_type_row;
 extern const o42a_gc_desc_t o42a_array_gc_desc;
 
 
+/**
+ * Allocates array.
+ *
+ * Allocates the necessary amount of memory to store an array in GC-collected
+ * block. An array data won't be initialized by this function.
+ *
+ * The block won't be submitted to the GC automatically. An array data should be
+ * filled before submitting to GC.
+ *
+ * \param value[out] an array value to fill.
+ * \param size[in]   the number of items the array should contain.
+ *
+ * \return pointer to array data, or NULL if array can not be allocated.
+ */
 o42a_array_t *o42a_array_alloc(o42a_val_t *, uint32_t);
 
+/**
+ * Copies one array value to another.
+ *
+ * \from[in] array value to copy from.
+ * \to[out]  array value to copy to.
+ */
 void o42a_array_copy(const o42a_val_t *, o42a_val_t *);
+
+/**
+ * Creates an array of duplicates.
+ *
+ * Allocates array and fills it with the same element.
+ *
+ * \param value[out] array value to fill.
+ * \param size[in]   the number of items the array should contain.
+ * \param item[in]   array item to fill array with.
+ */
+void o42a_array_of_duplicates(o42a_val_t *, uint32_t, o42a_array_t);
 
 void o42a_array_mark(const volatile o42a_val_t *);
 
