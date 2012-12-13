12bf5ccbdd95444c75a975e095d154237e7cf4d4
lorus <lorus@localhost>
2012-12-13 14:30:45 +0700

    Util: Move isDigit() methods to Cahracters class.

--- org.o42a.util/src/org/o42a/util/string/Characters.java
+++ org.o42a.util/src/org/o42a/util/string/Characters.java
@@ -27,6 +27,29 @@ public final class Characters {
 	public static final int HORIZONTAL_ELLIPSIS = 0x2026;
 	public static final int MINUS = 0x2212;
 
+	public static boolean isDigit(int c) {
+		return '0' <= c && c <= '9';
+	}
+
+	public static boolean isHexDigit(int c) {
+		if (c < '0') {
+			return false;
+		}
+		if (c <= '9') {
+			return true;
+		}
+		if (c > 'f' || c < 'A') {
+			return false;
+		}
+		if (c >= 'a') {
+			return true;
+		}
+		if (c <= 'F') {
+			return true;
+		}
+		return false;
+	}
+
 	private Characters() {
 	}
 
