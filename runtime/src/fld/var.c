095b00459e3032f9b6b5c2a65b084c0d042d05d8
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2013-01-01 17:40:18 +0700

    RT: Fix the debug info.

--- runtime/src/fld/var.c
+++ runtime/src/fld/var.c
@@ -13,7 +13,7 @@
 #ifndef NDEBUG
 const o42a_dbg_type_info2f_t _O42A_DEBUG_TYPE_o42a_fld_var = {
 	.type_code = 0x042a0200 | O42A_FLD_VAR,
-	.field_num = 4,
+	.field_num = 2,
 	.name = "o42a_fld_var",
 	.fields = {
 		{
