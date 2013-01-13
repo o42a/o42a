1d29ca355df05edc46fcb6f2d1b02cd59b2389c3
lorus <lorus@localhost>
2013-01-13 16:31:50 +0700

    Compiler: Move the `keep value` operator implementation to separate package.

--- org.o42a.compiler/src/org/o42a/compiler/ip/ref/keeper/KeptValue.java
+++ org.o42a.compiler/src/org/o42a/compiler/ip/ref/keeper/KeptValue.java
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-package org.o42a.compiler.ip.ref.operator;
+package org.o42a.compiler.ip.ref.keeper;
 
 import org.o42a.core.object.Obj;
 import org.o42a.core.object.ObjectMembers;
