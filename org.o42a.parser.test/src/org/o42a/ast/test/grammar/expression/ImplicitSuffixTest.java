38a1919c32a74c96e9284ee877cd0ea81486b0d4
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2013-03-13 16:40:26 +0700

    Grammar: A suffix operators can be omitted if its right operand starts with root reference ($$), similarly to the macros reference (##).

---
+++ org.o42a.parser.test/src/org/o42a/ast/test/grammar/expression/ImplicitSuffixTest.java
@@ -0,0 +1,83 @@
+/*
+    Any copyright is dedicated to the Public Domain.
+    http://creativecommons.org/publicdomain/zero/1.0/
+*/
+package org.o42a.ast.test.grammar.expression;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.o42a.parser.Grammar.expression;
+
+import org.junit.Test;
+import org.o42a.ast.expression.BinaryNode;
+import org.o42a.ast.expression.BinaryOperator;
+import org.o42a.ast.expression.PhraseNode;
+import org.o42a.ast.ref.MemberRefNode;
+import org.o42a.ast.ref.ScopeRefNode;
+import org.o42a.ast.ref.ScopeType;
+import org.o42a.ast.test.grammar.GrammarTestCase;
+
+
+public class ImplicitSuffixTest extends GrammarTestCase {
+
+	@Test
+	public void macroReference() {
+
+		final BinaryNode binary = parse("a ##b");
+
+		assertThat(binary.getOperator(), is(BinaryOperator.SUFFIX));
+		assertThat(binary.getSign(), hasRange(2, 4));
+		assertThat(binary.getLeftOperand(), isName("a"));
+
+		final MemberRefNode right =
+				to(MemberRefNode.class, binary.getRightOperand());
+
+		assertThat(right, hasName("b"));
+		assertThat(
+				to(ScopeRefNode.class, right.getOwner()).getType(),
+				is(ScopeType.MACROS));
+	}
+
+	@Test
+	public void rootReference() {
+
+		final BinaryNode binary = parse("a $$b");
+
+		assertThat(binary.getOperator(), is(BinaryOperator.SUFFIX));
+		assertThat(binary.getSign(), hasRange(2, 4));
+		assertThat(binary.getLeftOperand(), isName("a"));
+
+		final MemberRefNode right =
+				to(MemberRefNode.class, binary.getRightOperand());
+
+		assertThat(right, hasName("b"));
+		assertThat(
+				to(ScopeRefNode.class, right.getOwner()).getType(),
+				is(ScopeType.ROOT));
+	}
+
+	@Test
+	public void macroExpression() {
+
+		final BinaryNode binary = parse("a $$foo [bar]");
+
+		assertThat(binary.getOperator(), is(BinaryOperator.SUFFIX));
+		assertThat(binary.getSign(), hasRange(2, 4));
+		assertThat(binary.getLeftOperand(), isName("a"));
+
+		final PhraseNode phrase =
+				to(PhraseNode.class, binary.getRightOperand());
+		final MemberRefNode prefix =
+				to(MemberRefNode.class, phrase.getPrefix());
+
+		assertThat(prefix, hasName("foo"));
+		assertThat(
+				to(ScopeRefNode.class, prefix.getOwner()).getType(),
+				is(ScopeType.ROOT));
+	}
+
+	private BinaryNode parse(String text) {
+		return to(BinaryNode.class, parse(expression(), text));
+	}
+
+}
