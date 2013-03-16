e7445bb60001bf68e1b64ada2153d07f544c09d7
lorus <lorus@localhost>
2013-03-16 14:55:52 +0700

    Macros: Fix comparison macros definitions.

--- org.o42a.compiler.test/src/org/o42a/compiler/test/macro/ComparisonMacroTest.java
+++ org.o42a.compiler.test/src/org/o42a/compiler/test/macro/ComparisonMacroTest.java
@@ -12,9 +12,70 @@ public class ComparisonMacroTest extends CompilerTestCase {
 
 	@Test
 	public void eq() {
-		compile("A := 2 ##eq [2]");
+		compile(
+				"A := 2 ##eq [2]",
+				"B := 3 ##eq [2]");
 
 		assertTrueVoid(field("a"));
+		assertFalseVoid(field("b"));
+	}
+
+	@Test
+	public void ne() {
+		compile(
+				"A := 2 ##ne [3]",
+				"B := 2 ##ne [2]");
+
+		assertTrueVoid(field("a"));
+		assertFalseVoid(field("b"));
+	}
+
+	@Test
+	public void gt() {
+		compile(
+				"A := 3 ##gt [2]",
+				"B := 3 ##gt [3]",
+				"C := 2 ##gt [3]");
+
+		assertTrueVoid(field("a"));
+		assertFalseVoid(field("b"));
+		assertFalseVoid(field("c"));
+	}
+
+	@Test
+	public void ge() {
+		compile(
+				"A := 3 ##ge [2]",
+				"B := 3 ##ge [3]",
+				"C := 2 ##ge [3]");
+
+		assertTrueVoid(field("a"));
+		assertTrueVoid(field("b"));
+		assertFalseVoid(field("c"));
+	}
+
+	@Test
+	public void lt() {
+		compile(
+				"A := 2 ##lt [3]",
+				"B := 3 ##lt [3]",
+				"C := 3 ##lt [2]");
+
+		assertTrueVoid(field("a"));
+		assertFalseVoid(field("b"));
+		assertFalseVoid(field("c"));
+	}
+
+	@Test
+	public void le() {
+		compile(
+				"A := 2 ##le [3]",
+				"B := 3 ##le [3]",
+				"C := 3 ##le [2]");
+
+		assertTrueVoid(field("a"));
+		assertTrueVoid(field("b"));
+		assertFalseVoid(field("c"));
 	}
 
 }
