7ad9cc3c581bec37bd66de771d54ca2df7257148
Ruslan Lopatin <17871250+surol@users.noreply.github.com>
2013-05-08 17:39:44 +0700

    Parser Tests: Add a more complicated type arguments test.

--- org.o42a.parser.test/src/org/o42a/ast/test/grammar/type/TypeArgumentsTest.java
+++ org.o42a.parser.test/src/org/o42a/ast/test/grammar/type/TypeArgumentsTest.java
@@ -11,6 +11,7 @@ import static org.o42a.parser.Grammar.simpleExpression;
 
 import org.junit.Test;
 import org.o42a.ast.expression.ParenthesesNode;
+import org.o42a.ast.expression.PhraseNode;
 import org.o42a.ast.ref.RefNode;
 import org.o42a.ast.test.grammar.GrammarTestCase;
 import org.o42a.ast.type.AscendantsNode;
@@ -82,6 +83,21 @@ public class TypeArgumentsTest extends GrammarTestCase {
 		assertThat(ascendants.getSamples().length, is(1));
 		assertThat(ascendants.getAncestor().getSpec(), isName("bar"));
 		assertThat(ascendants.getSamples()[0].getSpec(), isName("baz"));
+	}
+
+	@Test
+	public void phrasePrefix() {
+
+		final PhraseNode phrase = to(
+				PhraseNode.class,
+				parse(simpleExpression(), "foo` bar [baz]"));
+		final TypeArgumentsNode args =
+				to(TypeArgumentsNode.class, phrase.getPrefix());
+
+		assertThat(phrase.getParts().length, is(1));
+		assertThat(args.getArguments().length, is(1));
+		assertThat(args.getArguments()[0].getArgument(), isName("foo"));
+		assertThat(args.getType(), isName("bar"));
 	}
 
 	private TypeArgumentsNode parse(String text) {
