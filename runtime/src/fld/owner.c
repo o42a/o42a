b0aa25cdcba12b70f14ac58e5c57a1dd320ff3e1
lorus <lorus@localhost>
2014-06-14 14:49:17 +0700

    IR: Rename scope field to owner field.

--- runtime/src/fld/owner.c
+++ runtime/src/fld/owner.c
@@ -9,25 +9,25 @@
 
 
 #ifndef NDEBUG
-const o42a_dbg_type_info1f_t _O42A_DEBUG_TYPE_o42a_fld_scope = {
-	.type_code = 0x042a0200 | O42A_FLD_SCOPE,
+const o42a_dbg_type_info1f_t _O42A_DEBUG_TYPE_o42a_fld_owner = {
+	.type_code = 0x042a0200 | O42A_FLD_OWNER,
 	.field_num = 1,
-	.name = "o42a_fld_scope",
+	.name = "o42a_fld_owner",
 	.fields = {
 		{
 			.data_type = O42A_TYPE_DATA_PTR,
-			.offset = offsetof(o42a_fld_scope, object),
+			.offset = offsetof(o42a_fld_owner, object),
 			.name = "object",
 		},
 	},
 };
 #endif /* NDEBUG */
 
-void o42a_fld_scope_propagate(o42a_obj_ctable_t *const ctable) {
+void o42a_fld_owner_propagate(o42a_obj_ctable_t *const ctable) {
 	O42A_ENTER(return);
 
-	const o42a_fld_scope *const from = &ctable->from.fld->scope;
-	o42a_fld_scope *const to = &ctable->to.fld->scope;
+	const o42a_fld_owner *const from = &ctable->from.fld->owner;
+	o42a_fld_owner *const to = &ctable->to.fld->owner;
 
 	// Update the scope only when propagating field.
 	if (ctable->to.body->flags & O42A_OBJ_BODY_PROPAGATED) {
@@ -39,7 +39,7 @@ void o42a_fld_scope_propagate(o42a_obj_ctable_t *const ctable) {
 		if (owner_data) {
 			to->object = O42A(o42a_obj_by_data(owner_data));
 
-			o42a_debug_mem_name("Updated scope: ", to->object);
+			o42a_debug_mem_name("Updated owner: ", to->object);
 
 			O42A_RETURN;
 		}
@@ -49,20 +49,20 @@ void o42a_fld_scope_propagate(o42a_obj_ctable_t *const ctable) {
 
 	to->object = from->object;
 
-	o42a_debug_mem_name("Leave the scope unchanged: ", to->object);
+	o42a_debug_mem_name("Leave the owner unchanged: ", to->object);
 
 	O42A_RETURN;
 }
 
-void o42a_fld_scope_inherit(o42a_obj_ctable_t *const ctable) {
+void o42a_fld_owner_inherit(o42a_obj_ctable_t *const ctable) {
 	O42A_ENTER(return);
 
-	const o42a_fld_scope *const from = &ctable->from.fld->scope;
-	o42a_fld_scope *const to = &ctable->to.fld->scope;
+	const o42a_fld_owner *const from = &ctable->from.fld->owner;
+	o42a_fld_owner *const to = &ctable->to.fld->owner;
 
 	to->object = from->object;
 
-	o42a_debug_mem_name("Leave the scope unchanged: ", from->object);
+	o42a_debug_mem_name("Leave the owner unchanged: ", from->object);
 
 	O42A_RETURN;
 }
